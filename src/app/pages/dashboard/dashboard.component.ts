d4fd97f85764231795bd46a23cbd9ee87b0e2de8
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2024-09-15 00:41:43 +0300

    feats : dashboar ve employees sayfaları düzenlendi ve veri istekleri artık state üzerinden yapılıyor.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,32 +1,39 @@
 import { Component, inject } from '@angular/core';
 import { DataService } from '../../shared/services/data.service';
 import { EmployeesComponent } from "../employees/employees.component";
+import { Store } from '@ngxs/store';
+import { EmployeeState } from '../../shared/store/EmployeeState';
+import { Observable } from 'rxjs';
+import { IEmployee } from '../../models/Employee';
+import { getAllEmployee } from '../../shared/store/EmployeeState';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-dashboard',
-    standalone: true,
-    templateUrl: './dashboard.component.html',
-    styleUrl: './dashboard.component.scss',
-    imports: [EmployeesComponent]
+  selector: 'app-dashboard',
+  standalone: true,
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss',
+  imports: [EmployeesComponent, CommonModule, MatButtonModule]
 })
+
 export class DashboardComponent {
   DataService = inject(DataService);
+  store = inject(Store);
+
+  Employees$: Observable<IEmployee[]> = inject(Store).select(EmployeeState.selectEmployee);
   totalEmployees: number = 0;
+  dashboardOpen: boolean = true;
 
   ngOnInit(): void {
-    this.getAllEmployee();
-  }
-
-  getAllEmployee() {
-    this.DataService.getAllEmployee().subscribe({
-      next: (data) => {
-        if (data.data) {
-          console.log(data.data);
-          this.totalEmployees = data.data?.length;
+    this.Employees$.subscribe({
+      next: (employees) => {
+        if (!employees.length) {
+          this.store.dispatch(new getAllEmployee());
         }
+        this.totalEmployees = employees.length;
       }
     })
   }
-
 
 }
--- src/app/pages/employees/employees.component.ts
+++ src/app/pages/employees/employees.component.ts
@@ -1,29 +1,35 @@
 import { Component, inject } from '@angular/core';
 import { DataService } from '../../shared/services/data.service';
 import { IEmployee } from '../../models/Employee';
+import { Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { EmployeeState, getAllEmployee } from '../../shared/store/EmployeeState';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-employees',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.scss'
 })
 export class EmployeesComponent {
-  employees: IEmployee[] = [];
   DataService = inject(DataService);
+  store = inject(Store);
+  Employees$: Observable<IEmployee[]> = inject(Store).select(EmployeeState.selectEmployee);
+  employees: IEmployee[] = [];
+  displayedColumns: string[] = ['name', 'email', 'mobile', 'dob', 'doj'];
+  dataSource = new MatTableDataSource<IEmployee>
 
   ngOnInit(): void {
-    this.getAllEmployee();
-  }
-
-  getAllEmployee() {
-    this.DataService.getAllEmployee().subscribe({
-      next: (data) => {
-        if (data.data) {
-          this.employees = data.data;
+    this.Employees$.subscribe({
+      next: (employees) => {
+        if (!employees.length) {
+          this.store.dispatch(new getAllEmployee());
         }
+        this.dataSource = new MatTableDataSource(employees);
       }
     })
   }
+
 }
